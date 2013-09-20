0868761d2e04fc89a3f0a54823af663a7b695026
HuvaaKoodia <hyvaakoodia@gmail.com>
2013-09-21 01:45:16 +0300

    Söldiers goto war

--- Assets/Game/Objects/SoldierMain.cs
+++ Assets/Game/Objects/SoldierMain.cs
@@ -11,6 +11,7 @@ public class SoldierMain : MonoBehaviour {
 	public bool AllyUnit;
 	
 	public bool DEAD{get;private set;}
+	public bool MOVING{get{return moving;}}
 	
 	int attack_power=10;
 	
@@ -33,17 +34,33 @@ public class SoldierMain : MonoBehaviour {
 	void Start () {
 		hp=100;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-			
-		if (Input.GetKeyDown(KeyCode.A)){
-			StartBlinking();
-		}
-	}
+
 	
 	public void UpdateTurn(){
 		
+		//check for enemies
+		for (int i=0;i<4;i++){
+			int xx=0,xy=0;
+			if (i==0)
+				xx=1;
+			if (i==1)
+				xx=-1;
+			if (i==2)
+				xy=1;
+			if (i==3)
+				xy=-1;
+			
+			SoldierMain unit=grid.GetUnit(x+xx,y+xy);
+			if (unit!=null){
+				if (AllyUnit!=unit.AllyUnit){
+					//attack
+					unit.Hit(attack_power);
+					return;
+				}
+			}
+		}
+		
+		//move
 		int tx=Target.x-x;
 		int ty=Target.y-y;
 		
@@ -77,36 +94,119 @@ public class SoldierMain : MonoBehaviour {
 		}
 		
 		if (!grid.GetPos(x+mx,y+my)){
-			//Move
-			SetPos(x+mx,y+my);
+			//Move 
+			//SetPos(x+mx,y+my);
+			Move(mx,my);
 		}
-		else{
-			SoldierMain unit=grid.GetUnit(x+mx,y+my);
-			if (unit!=null){
-				if (AllyUnit!=unit.AllyUnit){
-					//attack
-					unit.Hit(attack_power);
+	}
+	
+		
+	// Update is called once per frame
+	void Update () {
+			
+		if (Input.GetKeyDown(KeyCode.W)){
+			//StartBlinking();
+			Move(-1,0);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.S)){
+			//StartBlinking();
+			Move(1,0);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.A)){
+			//StartBlinking();
+			Move(0,-1);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.D)){
+			//StartBlinking();
+			Move(0,1);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.F)){
+			//StartBlinking();
+			StartBlinking();
+		}
+		
+		
+		if (moving){
+			float x_speed=0,z_speed=0;
+			
+			if (move_x<0){
+				x_speed=-1;
+				if (move_tx>transform.position.x){
+					moving=false;
+					SetWPos(x,y);
+				}
+			}
+			if (move_x>0){
+				x_speed=+1;
+				if (move_tx<=transform.position.x){
+					moving=false;
+					SetWPos(x,y);
+				}
+			}
+			if (move_y<0){
+				z_speed=-1;
+				if (move_ty>transform.position.z){
+					moving=false;
+					SetWPos(x,y);
+				}
+			}
+			if (move_y>0){
+				z_speed=+1;
+				if (move_ty<transform.position.z){
+					moving=false;
+					SetWPos(x,y);
 				}
 			}
 			
+			transform.position=new Vector3(transform.position.x+x_speed*Time.deltaTime,transform.position.y,transform.position.z+z_speed*Time.deltaTime);
 		}
-
 	}
 	
 	public void Hit(int power){
-		hp-=power;
+		HP-=power;
+		StartBlinking();
+	}
+	
+	float move_tx,move_ty,move_x,move_y;
+	bool moving=false;
+	
+	public void Move(int rx,int ry){
+		if (moving||grid.GetPos(x+rx,y+ry)) return;
+		StartCoroutine(MoveAnimation());
+		moving=true;
+		move_x=rx;
+		move_y=ry;
+	 	move_tx=x+rx+0.5f;
+		move_ty=y+ry+0.5f;
 		
+		SetGPos(x+rx,y+ry);
+	}
+	
+	IEnumerator MoveAnimation(){
+		yield return new WaitForSeconds(Random.Range(0,250)*0.001f);
+		graphics.animation.Play ();
 	}
 	
 	public void SetPos(int x,int y){
+		SetGPos(x,y);
+		SetWPos(x,y);
+	}
+	
+	public void SetGPos(int x,int y){
 		grid.ClearPos(this.x,this.y);
 		
 		grid.SetPos(x,y,true);
 		grid.SetUnit(x,y,this);
-		transform.position=new Vector3(x+0.5f,0,y+0.5f);
-		
 		this.x=x;this.y=y;
 		
+	}
+	
+	public void SetWPos(int x,int y){
+		transform.position=new Vector3(x+0.5f,0,y+0.5f);
 	}
 	
 	Color _color;
@@ -118,18 +218,12 @@ public class SoldierMain : MonoBehaviour {
 	}
 	
 	void StartBlinking(){
-		
-		StartCoroutine(Blink());
+		StopCoroutine("Blink");
+		graphics.renderer.material.color=_color;
+		StartCoroutine("Blink");
 	}
 	
 	IEnumerator Blink(){
-		while(true){
-			Debug.Log("asdasdasd!!");
-			yield return new WaitForSeconds(100);
-		}
-	}
-	
-	IEnumerator Blink2(){
 		for (int i=0;i<4;i++){
 			Color c=_color;
 			if (graphics.renderer.material.color==_color)
@@ -137,7 +231,13 @@ public class SoldierMain : MonoBehaviour {
 			
 			graphics.renderer.material.color=c;
 			
-			yield return new WaitForSeconds(100);
+			yield return new WaitForSeconds(0.1f);
 		}
+	}
+
+	public void DIE ()
+	{
+		grid.ClearPos(x,y);
+		Destroy(gameObject);
 	}
 }
--- Assets/Game/Sys/GameController.cs
+++ Assets/Game/Sys/GameController.cs
@@ -8,33 +8,41 @@ public class GameController : MonoBehaviour {
 	public GameObject soldier_prefab;
 	public TowerMain PlayerTower,EnemyTower;
 	
-	List<SoldierMain> player_units,enemy_units;
+	List<SoldierMain> units,player_units,enemy_units;
 	
 	// Use this for initialization
 	void Start () {
+		units=new List<SoldierMain>();
 		player_units=new List<SoldierMain>();
 		enemy_units=new List<SoldierMain>();
 		
 		PlayerTower.grid=grid;
 		EnemyTower.grid=grid;
 		
-		PlayerTower.SetPos(10,3,true);
+		PlayerTower.SetPos(10,2,true);
 		EnemyTower.SetPos(10,grid.GridHeight-3,false);
 		//player units
 		
-		for (int i=0;i<10;i++){
-			int x=Random.Range(0,grid.GridWidth);
-			int y=Random.Range(0,grid.GridHeight/2-2);
-		
+		for (int i=0;i<20;i++){
+			
+			int x,y;
+			do{
+				x=Random.Range(0,grid.GridWidth);
+				y=Random.Range(0,grid.GridHeight/2-2);
+			}
+			while(grid.GetPos(x,y));
 			var go=Instantiate(soldier_prefab,Vector3.zero,Quaternion.identity) as GameObject;
 			var sm=go.GetComponent<SoldierMain>();
 			
 			sm.grid=grid;
 			sm.Target=EnemyTower;
 			sm.SetPos(x,y);
-			player_units.Add(sm);
+			
 			sm.setColor(new Color(0.65f,0.65f,0.65f));
 			sm.AllyUnit=true;
+			
+			player_units.Add(sm);
+			units.Add(sm);
 		}
 		
 		//enemy units
@@ -49,23 +57,56 @@ public class GameController : MonoBehaviour {
 			sm.grid=grid;
 			sm.Target=PlayerTower;
 			sm.SetPos(x,y);
-			enemy_units.Add(sm);
 			sm.setColor(new Color(0.1f,0.1f,0.1f));
 			sm.AllyUnit=false;
+			
+			enemy_units.Add(sm);
+			units.Add(sm);
 		}
 	}
-	
+	bool turn_on=false,auto_turn;
 	// Update is called once per frame
-	void Update () {
-
-		if(Input.GetKeyDown(KeyCode.Space)){
-			for (int i=0;i<player_units.Count;i++){
-				player_units[i].UpdateTurn();	
-			}
+	void Update (){
+		
+		if (Input.GetKeyDown(KeyCode.Space)){
+			auto_turn=!auto_turn;
+		}
+		
+		if(auto_turn&&!turn_on){
 			
-			for (int i=0;i<enemy_units.Count;i++){
-				enemy_units[i].UpdateTurn();	
+			//update units
+			for (int i=0;i<units.Count;i++){
+				var s=units[i];
+				s.UpdateTurn();
 			}
+			//destroy units
+			
+			for (int i=0;i<units.Count;i++){
+				var s=units[i];
+
+				if (s.DEAD){
+					units.Remove(s);
+					player_units.Remove(s);
+					enemy_units.Remove(s);
+					s.DIE();
+					i--;
+				}
+			}
+			turn_on=true;
+			//auto_turn=false;
+		}
+		if (turn_on){
+			bool all_good=true;
+			for (int i=0;i<units.Count;i++){
+				var s=units[i];
+				if (s.MOVING){
+					all_good=false;
+					break;
+				}
+			}
+			if (all_good)
+				turn_on=false;
+			
 		}
 	}
 }
